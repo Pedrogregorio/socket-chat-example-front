6848e3dfd57864579939fa66d081dc335843a07b
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-11-22 19:49:49 -0400

    [NEW] select room

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home d-flex justify-center align-center">
+    <v-card
+      elevation="2"
+      class="mx-4"
+      width="500px"
+    >
+      <v-card-title class="justify-center">
+        Selecione uma sala
+      </v-card-title>
+      <v-card-text class="justify-center">
+        <v-text-field
+          v-model="username"
+          label="Apelido"
+          required
+        />
+        <v-select
+          v-model="room"
+          :items="rooms"
+          label="Sala"
+          required
+        />
+        <v-btn
+          color="primary"
+          dark
+          large
+          @click="doChat()"
+        >
+          Chat
+        </v-btn>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      username: '',
+      room: '',
+      rooms: [
+        { text: 'Sala 1', value: 'sala1' },
+        { text: 'Sala 2', value: 'sala2' },
+        { text: 'Sala 3', value: 'sala3' },
+      ],
+    }
+  },
+  methods: {
+    doChat() {
+      if (this.username && this.room) {
+        this.$router.push({
+          name: 'Chat',
+          params: {
+            username: this.username,
+            room: this.room,
+          },
+        })
+      }
+    },
   }
 }
 </script>
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  background-color: greenyellow;
+}
+</style>
