61293f21263d828695aa1d90e2d5312a4c62208c
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-12-25 09:11:39 -0400

    [NEW] create connection with socket

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import SocketIO from 'socket.io-client'
+
+import VueSocketIO from 'vue-socket.io'
+const socket = SocketIO('http://localhost:3000')
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: socket
+}));
 
 Vue.config.productionTip = false
 
